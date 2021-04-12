ce8e6af238583ee8b5bb6bae37c27efa11c7ccb7
sempc <ecaps@tistory.com>
2021-04-12 12:55:06 +0900

    2021-04-12 12:55:06.12

--- JqueryPro/WebContent/js/member/memberList.js
+++ JqueryPro/WebContent/js/member/memberList.js
@@ -1,84 +1,51 @@
 /**
- * 회원정보목록 JS
+ * 
  */
-
 $(document).ready(function(){
-	
 	$("#btnSearch").click(function(){
-//		event.preventDefault(); // <== submit 유형이라서 기본 동작을 막고 싶을때 필요할껄?
 		
-		var pmemId = $("#memId").val();
-		var pmemName = $("#memName").val();
-		var param = {memId: pmemId, memName: pmemName};
-//		var param = {memId: $("#memId").val(), memName: $("#memName").val()};
+		//서버로 전달할 데이터 만들기
+		var userId = $("#userId").val();
+		var userName = $("#userName").val();
+		
+		var param = {
+				memId : userId
+				, memName : userName
+				, flag : "L"
+				};
+		// ==> { memId : "test", memName : ""}
 		
 		$.ajax({
 			url : "/JqueryPro/MemberServlet"
 			,type : "post"
-//			,data : param
-			,data : $("#fm").serialize()
+			,data : param
 			,dataType : "json"
-			,success : function(data) {
-				alert('성공');
+			,success : function(data){
+				console.log(data);
+				// data를 이용해서 테이블을 만들기
 				makeTable(data);
+				
 			}
-			,error : function(xhr) {
-				alert("오류발생\n" + xhr.status);
+			,error : function(xhr){
 				console.log(xhr);
+				alert("오류발생");
 			}
 		});
-	})
-	
-	$("#tbResult").on("click", "a", function() {
-		goMemberView($(this).text());
+		
 	});
-})
+	
+});
 
 function makeTable(data){
-	var resList = data;
-	alert(resList);
+	var str = "";
 	
-	$("#tbResult tbody").remove();
-	
-	var html = "<tbody>";
-	if(resList == null || resList.length == 0) {
-			html = html
-					+ '<tr>'
-					+ '	<td colspan="8">검색 결과가 존재하지 않습니다.</td> '
-					+ '</tr>'
-					+ '</tbody>';
-	} else {
-		
-		
-		for(i in resList) {
-			var num = parseInt(i) + 1;
-			html = html
-					+ '	<tr>		   '
-					+ '		<td>' + num + '</td> '
-					+ '		<td><a href="#">' + resList[i].memId + '</a></td> '
-					+ '		<td>' + resList[i].memName + '</td> '
-					+ '		<td>' + resList[i].memPass + '</td> '
-					+ '		<td>' + resList[i].memBir + '</td> '
-//					+ '		<td>' + formatDate("yyyy-mm-dd", resList[i].birthday) + '</td> '
-					+ '		<td>' + resList[i].memHp + '</td> '
-//					+ '		<td>' + format("hpno", resList[i].memHp) + '</td> '
-					+ '		<td>' + resList[i].memMail + '</td> '
-					+ '		<td>' + resList[i].memJob + '</td> '
-					+ '	</tr>		  ';
-		}
-		html = html + '</tbody>';
+	for(var i=0 ; i<data.length ; i++) {
+		str += "<tr>"
+			+ "<td>" + data[i].id + "</td>"
+			+ "<td>" + data[i].name + "</td>"
+			+ "</tr>";
 	}
 	
-	$("#tbResult").append(html);
-	
-}
+	$("#tbResult tbody").html(str);
 
-function goMemberNew() {
-// 	window.location.href = "/html/member/memberNew.jsp";
-	
-	$("#targetUrl").val("/html/member/memberNew.jsp");
-	var fm = document.getElementById("tmpFm");
-	fm.method = "post";
-	fm.action = "/JqueryPro/PageServlet";
-	fm.submit();
 }
